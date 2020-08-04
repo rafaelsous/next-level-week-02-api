54b25d9721e455a911c693abcd9b401be9069393
rafaelsous <rafaelsousa.souza@gmail.com>
2020-08-04 14:02:12 -0300

    :wrench:

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,31 @@
 import express from 'express';
+import db from './database/connection';
 
 const router = express.Router();
 
-router.post('/classes', (request, response) => {
-  const data = request.body;
+router.post('/classes', async (request, response) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule,
+  } = request.body;
 
-  console.log(data);
+  const { user_id } = await db('users').insert({
+    name,
+    avatar,
+    whatsapp,
+    bio,
+  });
+
+  await db('classes').insert({
+    subject,
+    cost,
+    user_id,
+  });
 
   response.status(201).send();
 });
